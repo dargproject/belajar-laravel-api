8b3407ecc9656877c6421591601cd939d0e380db
dargproject <dnny.agra@gmail.com>
2023-10-18 16:12:02 +0700

    fearute: api crud users

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\UserStoreRequest;
+use App\Http\Requests\User\UserUpdateRequest;
+use App\Http\Resources\User\UserIndexResource;
+use App\Http\Resources\User\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user = UserIndexResource::collection(
+            User::query()->paginate(5)
+        );
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil ditampilan',
+            'data' => $user->items(),
+            'meta' => [
+                'total' => $user->total(),
+                'per_page' => $user->perPage(),
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserStoreRequest $request)
+    {
+
+        $user = User::create($request->validated());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil ditambahkan',
+            'data' => new UserResource($user)
+
+        ], Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil ditampilkan',
+            'data' => new UserResource($user)
+
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UserUpdateRequest $request, User $user)
+    {
+        $user->update($request->validated());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil Diubah',
+            'data' => new UserResource($user)
+
+        ]);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Data Berhasil Dihapus',
+            'data' => []
+        ], Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'arga@gmail.com',
+            'password' => bcrypt('123456'),
+            'is_admin' => true,
+        ]);
+        User::factory()->count(50)->create();
+    }
+}
